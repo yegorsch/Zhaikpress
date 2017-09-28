faad10b706e6b20da50766b0142b1465e68303d1
Yegor Chsherbatykh <yegor.chsherbatykg@nu.edu.kz>
2017-09-29 00:36:44 +0600

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Егор on 9/29/17.
+//  Copyright © 2017 Yegor's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
